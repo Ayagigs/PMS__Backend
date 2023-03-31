a8763b42f1b3c9aca9dd434c66ebbb2483c6306a
Sophia Okosodo <sophieokosodo@gmail.com>
2023-03-31 15:20:33 +0100

    updating options

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -3,8 +3,25 @@ import Admin from "../model/adminModel.js";
 
 export const addQuestion = async(req, res) => {
     const {questions, options, reviewType, category} = req.body;
+    const {companyID} = req.params;
 
+    
     try{
+        const questionFound = await Question.findOne({reviewType, category})
+        if(questionFound){
+            const question = await Question.findOneAndUpdate({companyID, reviewType, category}, {
+                $set: {
+                    questions
+                }
+            },
+            {
+                new: true
+            })
+
+            return res.status(200).send({status: 'Success', data: question})
+        }
+
+
         const question = await Question.create({
             questions,
             options,
@@ -79,4 +96,47 @@ export const getAllQuestion = async(req, res) => {
         res.status(500).send({status: 'Success', message: error.message})
     }
 }
+
+export const getCompetencyQuestion = async(req, res) => {
+    const {companyID} = req.params;
+    
+    try{
+        const questions = await Question.findOne({companyID, category: "Competency"})
+    
+        const sortedQuestion = questions.questions.sort((a, b) => {
+            return a.order - b.order;
+        });
+        const sortedOption = questions.options.sort((a, b) => {
+            return b.value - a.value;
+        });
+    
+        res.status(200).send({status: 'Success', data: {sortedQuestion, sortedOption}})
+    
+    }catch(error){
+        res.status(500).send({status: 'Success', message: error.message})
+    }
+
+}
+export const updateOption  = async(req, res) => {
+    const {companyID} = req.params;
+    const {options} = req.body;
+    
+    try{
+        const questions = await Question.find({companyID})
+
+        for(let i = 0; i < questions.length; i++){
+            questions[0].options = options
+
+            await questions[0].save()
+        }
+    
+        res.status(200).send({status: 'Success', data: {options}})
+    
+    }catch(error){
+        res.status(500).send({status: 'Success', message: error.message})
+    }
+
+}
+
+
 
